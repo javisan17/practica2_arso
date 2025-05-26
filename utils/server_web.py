352536b0fecfbcf790f4fbd3f889b201733baecb
Enrique Galindo <kikesco1ce@gmail.com>
2025-05-26 11:56:51 +0200

    Depuración ip_files

--- utils/server_web.py
+++ utils/server_web.py
@@ -86,13 +86,18 @@ def change_ip_files(name, ip):
         
         #Reemplazar IP en rest_server.js
         #subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s/^await mongoose.connect('mongodb://134.3.0.20/bio_bbdd',{{ useNewUrlParser: true, useUnifiedTopology: true }})$/await mongoose.connect('mongodb:/{ip}//bio_bbdd',{{ useNewUrlParser: true, useUnifiedTopology: true }})", "/app/rest_server.js"], check=True)
-        subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s|mongodb://[0-9.]\+/bio_bbdd|mongodb://{ip}/bio_bbdd|", "/root/app/rest_server.js"], check=True)
+        #subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s|mongodb://[0-9.]\+/bio_bbdd|mongodb://{ip}/bio_bbdd|", "/root/app/rest_server.js"], check=True)
+        subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s|mongodb://[^/]\+/bio_bbdd|mongodb://{ip}/bio_bbdd|", "/root/app/rest_server.js"], check=True)
 
         #Reemplazar IP en md-seed-config.js
         #subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s/^const mongoURL = process.env.MONGO_URL || 'mongodb://134.3.0.20:27017/bio_bbdd';$/const mongoURL = process.env.MONGO_URL || 'mongodb://{ip}:27017/bio_bbdd';", "/app/md-seed-config.js"], check=True)
-        subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s|mongodb://[0-9.]\+:27017/bio_bbdd|mongodb://{ip}:27017/bio_bbdd|", "/root/app/md-seed-config.js"], check=True)
+        subprocess.run(["lxc", "exec", name, "--", "sed", "-i", f"s|mongodb://[^:]\+:27017/bio_bbdd|mongodb://{ip}:27017/bio_bbdd|", "/root/app/md-seed-config.js"], check=True)
 
         logger.info("IP cambiada correctamente en los archivos de configuración.")
+        
+        #Reiniciar contenedor
+        logger.debug(f"Reiniciando contenedor {name}")
+        subprocess.run(["lxc", "restart", name], check=True)
 
     except subprocess.CalledProcessError as e:
         logger.error(f"Error al cambiar la IP en los archivos del contenedor {name}: {e}", exc_info=True)
